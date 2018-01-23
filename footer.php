d978fff627e8af98b35f9f7c9d5cfb77b90fe4e0
budiraharjo <boeday604@gmail.com>
2018-01-23 13:29:33 +0700

    version 1.0

---
+++ footer.php
@@ -0,0 +1,8 @@
+<div class="container">
+	<div class="alert alert-info">
+    <strong>tutorial link !</strong> <a href="">cleartuts</a>!
+	</div>
+</div>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
